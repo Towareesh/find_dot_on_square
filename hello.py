b9d5b67c6083c8539260aab144e58c99a920acdc
Towareesh <pakman.dir6016@gmail.com>
2022-02-23 23:05:20 +0200

    h3123ffd

---
+++ hello.py
@@ -0,0 +1,18 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/index')
+def hello_world():
+    user = {'username': 'чел'}
+    return '''
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
+
+if __name__ == '__main__':
+    app.run(debug=True)
