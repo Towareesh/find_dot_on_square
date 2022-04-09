75c5f0a1f29a8b5a88533576daa5c92cbacb17e4
Towareesh <63032856+Towareesh@users.noreply.github.com>
2022-04-09 17:14:08 +0200

    step

--- hello.py
+++ hello.py
@@ -1,22 +1,12 @@
-from flask import Flask
-app = Flask(__name__)
+class NewClass:
+    """docstring for NewClass"""
+    def __init__(self, arg):
+        """magic method"""
+        self.arg = arg
+    
+    def main(self, arg):
+        return 'Hi'
 
-
-
-@app.route('/index')
-def hello_world():
-    user = {'username': 'чел'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+a = 'hLLO'
+rec = NewClass(a).main(a)
+print(rec)
